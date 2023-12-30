5bb11730596842a0781407e22771223582fba985
padhibailochan19 <padhibailochan19@gmail.com>
2023-12-30 15:01:12 +0530

    first commit

---
+++ src/test/java/com/bailochan/serviceregistryproject/ServiceRegistryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bailochan.serviceregistryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
